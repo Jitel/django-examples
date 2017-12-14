746212cacce7aa9d844698f82b04f8c44a66ee30
Jitel <git.zamkovec@bk.ru>
2017-12-14 14:36:12 +0300

    first commit

---
+++ mysite/mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index (request):
+    return render(request, 'mainapp/homePage.html')
+
+def contact (request):
+    return render(request, 'mainapp/basic.html', {'values':['Если есть вопросы, задайте их по номеру', '+375256208265' ]})
+
+
