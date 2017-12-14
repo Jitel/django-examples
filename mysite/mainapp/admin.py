4d72ae15456e438311af0f7f481517e3b72090f1
Jitel <git.zamkovec@bk.ru>
2017-12-14 16:59:07 +0300

    second commit

--- mysite/mainapp/admin.py
+++ mysite/mainapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+#from mysite.mainapp.models import Topic
 
-# Register your models here.
+#admin.site.register(Topic)
+
--- mysite/mainapp/urls.py
+++ mysite/mainapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from . import views
 urlpatterns =[
     url(r'^$', views.index, name='index'),
-    url(r'^contact/&', views.contact, name='contact'),
+    url(r'^contact/', views.contact, name='contact'),
+    url(r'^news/', views.news, name='news'),
 
 ]
